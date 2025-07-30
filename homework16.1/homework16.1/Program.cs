ab9d24b87e93d2033ffe493c5c1d86b51df188d6
Oleksandr <sanyalisnyak@gmail.com>
2025-07-30 17:01:40 +0300

    add homework16.1

---
+++ homework16.1/homework16.1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the path to the source file: ");
+        string sourcePath = Console.ReadLine();
+
+        Console.Write("Enter the path to the file you want to copy the data to.: ");
+        string destinationPath = Console.ReadLine();
+
+        try
+        {
+            File.Copy(sourcePath, destinationPath); 
+            Console.WriteLine("File copied successfully!");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found. Check the path.");
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine("File or path not accessible.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
